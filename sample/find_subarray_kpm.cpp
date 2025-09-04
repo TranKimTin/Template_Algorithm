9d7f95ecfd72f22f60662926cad2e5842f096c73
Trần Kim Tín <kimtin.tr@gmail.com>
2025-09-04 23:07:39 +0700

    Update find_subarray_kpm.cpp

--- sample/find_subarray_kpm.cpp
+++ sample/find_subarray_kpm.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 
 // Tính mảng LPS (Longest Prefix Suffix) cho pattern
+// pi[i] định nghĩa: độ dài tiền tố dài nhất = hậu tố của s[0..i].
 vector<int> computeLPS(const vector<int> &pattern)
 {
     int m = pattern.size();
