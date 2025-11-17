f8bc0c445c2ed876a4feba38f568bac28803a8c8
tin.tran <tin.tran@lge.com>
2025-11-17 21:26:33 +0700

    lca update

--- sample/LCA_to_tien_chung_gan_nhat.cpp
+++ sample/LCA_to_tien_chung_gan_nhat.cpp
@@ -1,123 +1,124 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define pii pair<int, int>
+#define ll long long
+#define MAX 1000
+#define MAX_LOG_N 20
 
-class WeightedTree
+int n;
+vector<pii> a[MAX];
+int up[MAX][MAX_LOG_N];
+int depth[MAX];
+ll d[MAX];
+int logN;
+
+void init()
 {
-private:
-    int n, logN;
-    vector<vector<pair<int, int>>> a;
-    vector<vector<int>> up;
-    vector<int> depth;
-    vector<long long> d;
+    logN = 0;
+    while ((1 << logN) <= n + 1)
+        ++logN;
 
-    void dfs(int u, int parent)
+    for (int i = 0; i <= n; i++)
     {
-        up[u][0] = parent;
-        for (int i = 1; i < logN; ++i)
+        fill(up[i], up[i] + logN, -1);
+    }
+    fill(depth, depth + n + 1, 0);
+    fill(d, d + n + 1, 0);
+}
+
+
+void f(int u, int parent)
+{
+    up[u][0] = parent;
+    for (int i = 1; i < logN; ++i)
+    {
+        if (up[u][i - 1] != -1)
+            up[u][i] = up[up[u][i - 1]][i - 1];
+        else
+            up[u][i] = -1;
+    }
+
+    for (auto [v, w] : a[u])
+    {
+        if (v != parent)
         {
-            if (up[u][i - 1] != -1)
-                up[u][i] = up[up[u][i - 1]][i - 1];
-            else
-                up[u][i] = -1;
+            depth[v] = depth[u] + 1;
+            d[v] = d[u] + w;
+            f(v, u);
         }
+    }
+}
 
-        for (auto [v, w] : a[u])
+int kth_ancestor(int u, int k)
+{
+    for (int i = 0; i < logN; ++i)
+    {
+        if (k & (1 << i))
         {
-            if (v != parent)
-            {
-                depth[v] = depth[u] + 1;
-                d[v] = d[u] + w;
-                dfs(v, u);
-            }
+            u = up[u][i];
+            if (u == -1)
+                break;
         }
     }
+    return u;
+}
 
-public:
-    WeightedTree(int maxNode)
-    {
-        n = maxNode + 1;
-        logN = 0;
-        while ((1 << logN) <= n)
-            ++logN;
+int lca(int u, int v)
+{
+    if (depth[u] < depth[v])
+        swap(u, v);
+    u = kth_ancestor(u, depth[u] - depth[v]);
 
-        a.resize(n);
-        up.assign(n, vector<int>(logN, -1));
-        depth.assign(n, 0);
-        d.assign(n, 0);
-    }
-
-    void add_edge(int u, int v, int w)
-    {
-        a[u].emplace_back(v, w);
-        a[v].emplace_back(u, w);
-    }
-
-    void build(int root = 1)
-    {
-        dfs(root, -1);
-    }
-
-    int kth_ancestor(int u, int k)
-    {
-        for (int i = 0; i < logN; ++i)
-        {
-            if (k & (1 << i))
-            {
-                u = up[u][i];
-                if (u == -1)
-                    break;
-            }
-        }
+    if (u == v)
         return u;
-    }
 
-    int lca(int u, int v)
+    for (int i = logN - 1; i >= 0; --i)
     {
-        if (depth[u] < depth[v])
-            swap(u, v);
-        u = kth_ancestor(u, depth[u] - depth[v]);
-
-        if (u == v)
-            return u;
-
-        for (int i = logN - 1; i >= 0; --i)
+        if (up[u][i] != -1 && up[u][i] != up[v][i])
         {
-            if (up[u][i] != -1 && up[u][i] != up[v][i])
-            {
-                u = up[u][i];
-                v = up[v][i];
-            }
+            u = up[u][i];
+            v = up[v][i];
         }
-        return up[u][0];
     }
+    return up[u][0];
+}
 
-    long long distance(int u, int v)
-    {
-        int ancestor = lca(u, v);
-        return d[u] + d[v] - 2 * d[ancestor];
-    }
-};
+ll distance(int u, int v)
+{
+    int ancestor = lca(u, v);
+    return d[u] + d[v] - 2 * d[ancestor];
+}
 
 int main()
 {
-    int n;
-    cin >> n;
-    WeightedTree tree(n);
+    n = 5;
 
-    for (int i = 1; i < n; ++i)
+
+    tuple<int, int, int> edges[n - 1] = {{2, 3, 100}, {4, 3, 200}, {1, 5, 150}, {1, 3, 50}};
+    for (int i = 0; i < n - 1; i++)
     {
-        int u, v, w;
-        cin >> u >> v >> w;
-        tree.add_edge(u, v, w);
+        auto [u, v, w] = edges[i];
+        a[u].push_back({v, w});
+        a[v].push_back({u, w});
     }
+    
+    init();
+    f(1, -1); // gốc là node 1
 
-    tree.build(0); // gốc là node 0
+    for (int u = 1; u <= n; u++)
+    {
+        for (int k = 0; k < 3; k++)
+        {
+            cout << "Ancestor(" << u << ", " << k << ") = " << kth_ancestor(u, k) << '\n';
+        }
 
-    int u, v, k;
-    cin >> u >> v >> k;
-    cout << "LCA(" << u << ", " << v << ") = " << tree.lca(u, v) << '\n';
-    cout << "Ancestor(" << u << ", " << k << ") = " << tree.kth_ancestor(u, k) << '\n';
-    cout << "Distance(" << u << ", " << v << ") = " << tree.distance(u, v) << '\n';
+        for (int v = u + 1; v <= n; v++)
+        {
+            cout << "LCA(" << u << ", " << v << ") = " << lca(u, v) << '\n';
+            cout << "Distance(" << u << ", " << v << ") = " << distance(u, v) << '\n';
+        }
+        cout << endl;
+    }
 
     return 0;
 }
